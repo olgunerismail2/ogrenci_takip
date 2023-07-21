8fcdfc6b5f0b4afdc5501e1fcb7e7ce7dc8a38d3
İsmail <olgunerismail2@gmail.com>
2023-07-22 00:07:54 +0300

    flexible yapısı

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,22 @@ import 'package:kurs/models/student.dart';
 void main() {
   runApp(MaterialApp(home: MyApp()));
 }
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   String mesaj = "Öğrenci Takip Sistemi";
-  List<Student> students=[new Student("İsmail", "Olguner", 20),Student("Alim", "Ulaş", 80),Student("Halil", "Duymaz", 45)];
+
+  String seciliOgrenci = "abc";
+
+  List<Student> students = [
+    new Student("İsmail", "Olguner", 20),
+    Student("Alim", "Ulaş", 80),
+    Student("Halil", "Duymaz", 45)
+  ];
 
   var ogrenciler = [
     "İsmail Olguner",
@@ -14,9 +27,9 @@ class MyApp extends StatelessWidget {
     "Berkay Bilgin",
     "Murat Kurt"
   ];
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text(mesaj),
@@ -37,7 +50,7 @@ class MyApp extends StatelessWidget {
     return mesaj;
   }
 
-  void mesajGoster(BuildContext context,String mesaj) {
+  void mesajGoster(BuildContext context, String mesaj) {
     var alert = AlertDialog(
       title: Text("Sınav Sonucu"),
       content: Text(mesaj),
@@ -47,8 +60,7 @@ class MyApp extends StatelessWidget {
   }
 
   Widget buidBody(BuildContext context) {
-
-   return Column(
+    return Column(
       children: <Widget>[
         Expanded(
             child: ListView.builder(
@@ -56,38 +68,111 @@ class MyApp extends StatelessWidget {
                 itemBuilder: (BuildContext context, int index) {
                   return ListTile(
                     leading: CircleAvatar(
-                      backgroundImage: NetworkImage("https://pixabay.com/tr/photos/adam-iskele-siluet-g%C3%BCndo%C4%9Fumu-sis-8091933/"),
+                      backgroundImage: NetworkImage(
+                          "https://pixabay.com/tr/photos/adam-iskele-siluet-g%C3%BCndo%C4%9Fumu-sis-8091933/"),
                     ),
-                    title: Text(students[index].firstName + " " + students[index].lastName),
-                    subtitle: Text("Sınavdan aldığı not : " + students[index].grade.toString() + " ["+students[index].getStatus+"]"),
+                    title: Text(students[index].firstName +
+                        " " +
+                        students[index].lastName),
+                    subtitle: Text("Sınavdan aldığı not : " +
+                        students[index].grade.toString() +
+                        " [" +
+                        students[index].getStatus +
+                        "]"),
                     trailing: buildStatusIcon(students[index].grade),
-                    onTap: (){
-                      print(students[index].firstName + " " + students[index].lastName);
+                    onTap: () {
+                      setState(() {
+                        seciliOgrenci = students[index].firstName +
+                            " " +
+                            students[index].lastName;
+                      });
+
+                      print(seciliOgrenci);
                     },
                   );
                 })),
-        Center(
-          child: ElevatedButton(
-            child: Text("Sonucu Gör !!!!"),
-            onPressed: () {
-              var mesaj=  sinavHesapla(35);
-              mesajGoster(context,mesaj);
-            },
-          ),
-        ),
+        Text("Seçili Öğrenci : " + seciliOgrenci),
+        Row(
+          children: <Widget>[
+            Flexible(
+              fit: FlexFit.tight,
+              flex: 1,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.purpleAccent,
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.add),
+                    SizedBox(
+                      width: 5.0,
+                    ),
+                    Text("Yeni Öğrenci"),
+                  ],
+                ),
+                onPressed: () {
+                  var mesaj = sinavHesapla(35);
+                  mesajGoster(context, mesaj);
+                },
+              ),
+            ),
+            Flexible(
+              fit: FlexFit.tight,
+              flex: 1,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.amberAccent,
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.update),
+                    SizedBox(
+                      width: 5.0,
+                    ),
+                    Text("Güncelle"),
+                  ],
+                ),
+                onPressed: () {
+                  var mesaj = sinavHesapla(35);
+                  mesajGoster(context, mesaj);
+                },
+              ),
+            ),
+            Flexible(
+              fit: FlexFit.tight,
+              flex: 1,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.greenAccent,
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.delete),
+                    SizedBox(
+                      width: 5.0,
+                    ),
+                    Text("Sil"),
+                  ],
+                ),
+                onPressed: () {
+                  var mesaj = sinavHesapla(35);
+                  mesajGoster(context, mesaj);
+                },
+              ),
+            ),
+          ],
+        )
       ],
     );
   }
 
-Widget buildStatusIcon(int grade) {
- if (grade >= 50) {
+  Widget buildStatusIcon(int grade) {
+    if (grade >= 50) {
       return Icon(Icons.done);
     } else if (grade >= 40) {
-   return Icon(Icons.album);
+      return Icon(Icons.album);
     } else {
-   return Icon(Icons.clear);
+      return Icon(Icons.clear);
     }
-
   }
-
 }
